90b22db98aa4ba20ce762573c3a20adfb06ec637
Rob Pethick <rpethick@proximie.com>
2024-06-14 16:27:07 +0100

    New parse brackets question

---
+++ parse-brackets.ts
@@ -0,0 +1,7 @@
+// Write a function that can validate if the brackets in a string are correctly matched and closed.
+// This should consider round (), square [], and curly {} brackets.
+
+// Your code goes here!
+export const execute = (input: string): boolean => {
+  throw new Error("Remove this statement and implement this function");
+};
