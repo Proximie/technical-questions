a4a50b2bb65b0b869b8881ffb3aed8095b09ff8b
Rob Pethick <rpethick@proximie.com>
2024-06-18 22:05:01 +0100

    Add new starter question

---
+++ capitalise-words.ts
@@ -0,0 +1,6 @@
+// Write a function that takes an input string and capitalises the first letter only of each word.
+
+// Your code goes here!
+export const execute = (input: string): boolean => {
+  throw new Error("Remove this statement and implement this function");
+};
